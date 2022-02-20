fc7fa61c7b96c07e7212be521b859c51cbbfa41b
Zhuravlev Dmitriy <absolutelynormal2@gmail.com>
2022-02-20 17:29:54 +0300

    Test commit

---
+++ src/test/java/com/example/LionParameterizedTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+
+public class LionParameterizedTest {
+
+
+
+}
