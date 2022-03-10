cca3b770b5d2bb8b804691d17a10d6eaa8ca5b27
Zhuravlev Dmitriy <absolutelynormal2@gmail.com>
2022-03-11 02:04:33 +0300

    Commit Two

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,9 +1,6 @@
 package com.example;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
-import org.mockito.Spy;
 import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -13,9 +10,6 @@ public class FelineTest {
 
     private Feline feline;
 
-    @Spy
-    private Feline felineSpy = new Feline();
-
     @Before
     public void setUp() {
         feline = new Feline();
@@ -24,8 +18,7 @@ public class FelineTest {
     @Test
     public void eatMeatReturnCorrectValueTest() throws Exception {
         List<String> expectedListFood =  List.of("Животные", "Птицы", "Рыба");
-        Mockito.when(felineSpy.getFood("Хищник")).thenReturn(expectedListFood);
-        List<String> actualListFood = felineSpy.eatMeat();
+        List<String> actualListFood = feline.eatMeat();
         assertThat(expectedListFood, containsInAnyOrder(actualListFood.toArray()));
     }
 
@@ -41,13 +34,6 @@ public class FelineTest {
         int expectedKittensCount = 1;
         int actualKittensCount = feline.getKittens();
         assertEquals(expectedKittensCount, actualKittensCount);
-    }
-
-    @Test
-    public void getFamilyCorrectString() {
-        String expectedFamilyString = "Кошачьи";
-        String actualFamilyString = feline.getFamily();
-        assertEquals(expectedFamilyString, actualFamilyString);
     }
 
 }
--- src/test/java/com/example/LionParameterizedTest.java
+++ src/test/java/com/example/LionParameterizedTest.java
@@ -1,15 +1,10 @@
 package com.example;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import java.util.List;
 import static org.junit.Assert.assertEquals;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
+
 
 @RunWith(Parameterized.class)
 public class LionParameterizedTest {
@@ -20,7 +15,7 @@ public class LionParameterizedTest {
 @Mock
 Feline feline;
 
-public LionParameterizedTest(String sex, Boolean expected) {
+public LionParameterizedTest(String sex, boolean expected) {
     this.sex = sex;
     this.expected = expected;
 }
@@ -33,11 +28,6 @@ public LionParameterizedTest(String sex, Boolean expected) {
         };
     }
 
-    @Before
-    public void init() {
-        MockitoAnnotations.openMocks(this);
-    }
-
     @Test
     public void getDoesHaveManeLion() throws Exception {
         Lion lion = new Lion(sex, feline);
@@ -45,17 +35,4 @@ public LionParameterizedTest(String sex, Boolean expected) {
         assertEquals(expected, actual);
     }
 
-    @Test
-    public void getKittensFelineCorrectValue() {
-        int expectedKittensCount = 1;
-        int actualKittensCount = feline.getKittens();
-        assertEquals(expectedKittensCount, actualKittensCount);
-    }
-
-    @Test
-    public void getFamilyCorrectString() {
-        String expectedFamilyString = "Кошачьи";
-        String actualFamilyString = feline.getFamily();
-        assertEquals(expectedFamilyString, actualFamilyString);
-    }
 }
